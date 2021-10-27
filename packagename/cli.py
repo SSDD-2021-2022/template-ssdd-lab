78133e7d92f7da253ec5bb67a321f874b2041cb7
José Luis Segura Lucas <profesor.jlsegura@uclm.es>
2021-10-27 22:28:24 +0200

    Initial commit:
    - Adding README.md
    - Add CODEOWNERS
    - Adding gitignore and fixing permissions
    - Adding a GH action

---
+++ packagename/cli.py
@@ -0,0 +1,27 @@
+"""Submodule containing the CLI command handlers."""
+
+import sys
+
+
+def main_service():
+    """Handles the `mainservice` CLI command."""
+    print("Main service")
+    sys.exit(0)
+
+
+def catalog_service():
+    """Handles the `catalogservice` CLI command."""
+    print("Catalog service")
+    sys.exit(0)
+
+
+def streaming_service():
+    """Handles the `streamingservice` CLI command."""
+    print("Streaming service")
+    sys.exit(0)
+
+
+def authentication_service():
+    """Handles the `authenticationservice` CLI command."""
+    print("Authentication service")
+    sys.exit(0)
